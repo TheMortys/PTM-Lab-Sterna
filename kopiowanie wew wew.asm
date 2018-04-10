29907e0ca292abbcbe4d844e4f501014f3d81231
mrnevea <aninik9@gmail.com>
2018-04-10 17:14:44 +0200

    Dodanie opisy w komentarzach

--- kopiowanie wew wew.asm
+++ kopiowanie wew wew.asm
@@ -1,20 +1,20 @@
 ORG 0
 
-	MOV R0, #30h
-	MOV R1, #40h
-	MOV R2, #8
+	MOV R0, #30h ;adres zrodlowy
+	MOV R1, #40h ;adres docelowy
+	MOV R2, #8 ;ilosc bitow do skopiowania (ilosc obiegów petli)
 	LCALL COPY
 	SJMP $
 		
 		COPY:
-		MOV A, @R0
-		MOV @R1, A
-		INC R0
-		INC R1
-		DEC R2
-		MOV A, R2
+		MOV A, @R0 ;przeniesienie wartosci z R0 do akumulatora
+		MOV @R1, A ;przeniesienie wartosci z akumulatora do R1
+		INC R0 ;inkrementacja wskaznika zródlowego
+		INC R1 ;inkrementacja wskaznika docelowego
+		DEC R2 ;dekrementacja R2
+		MOV A, R2 ;wpisanie R2 do akumulatora
 		
-		JNZ COPY
+		JNZ COPY ;wykonywanie kopiowania dopóki A>0
 		
 		RET
 		
