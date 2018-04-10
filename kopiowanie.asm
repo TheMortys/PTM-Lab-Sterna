89178bca070c8291b65f65d3d99a6008027e6d81
mrnevea <aninik9@gmail.com>
2018-04-10 14:56:59 +0200

    Dodanie kopiowania wew->wew

---
+++ kopiowanie.asm
@@ -0,0 +1,21 @@
+ORG 0
+
+	MOV R0, #30h
+	MOV R1, #40h
+	MOV R2, #8
+	LCALL COPY
+	SJMP $
+		
+		COPY:
+		MOV A, @R0
+		MOV @R1, A
+		INC R0
+		INC R1
+		DEC R2
+		MOV A, R2
+		
+		JNZ COPY
+		
+		RET
+		
+END
